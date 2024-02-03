65e2c359cad8e99c1ffba248005ffd53b035be80
haoxiang01 <276063373@qq.com>
2024-02-03 22:27:25 +0000

    Update plot

--- Assignment1/Task1/Task1_Impulse.m
+++ Assignment1/Task1/Task1_Impulse.m
@@ -24,41 +24,41 @@ x(1) = 10000; % Impulse at t=0
 
 % ACF and PSD Calculation
 [acf_x, lags] = xcorr(x, 'biased'); % ACF
-[pxx1_x, f_pxx1] = fPSD(x, Fs,'def1');% PSD def1
+[pxx1_x, f_pxx1] = fPSD(acf_x, Fs,'def1');% PSD def1
 [pxx2_x, f_pxx2] = fPSD(x, Fs,'def2');% PSD def2
 
 %Plotting
 figure;
 
 % Signal sequence
-subplot(4,1,1)
-plot(t, x, 'LineWidth', 1.5);
+subplot(3,1,1)
+plot(t, x, 'b','LineWidth',  2);
 title('Signal Sequence $x_1(t)$', 'Interpreter', 'latex');
 xlabel('Time (s)', 'Interpreter', 'latex');
 ylabel('Amplitude', 'Interpreter', 'latex');
 grid on;
 
 % ACF
-subplot(4,1,2)
-plot(lags*T, acf_x, 'LineWidth', 1.5);
+subplot(3,1,2)
+plot(lags*T, acf_x,'b', 'LineWidth', 2);
 title('ACF of Impulse Signal $x_1(t)$', 'Interpreter', 'latex');
 xlabel('Time lag (s)', 'Interpreter', 'latex');
 ylabel('Amplitude', 'Interpreter', 'latex');
 grid on;
 
 % PSD def1
-subplot(4,1,3)
-plot(f_pxx1, 10*log10(pxx1_x), 'LineWidth', 1.5);
-title('Def-1 PSD of Impulse Signal $x_1(t)$', 'Interpreter', 'latex');
-xlabel('Frequency (Hz)', 'Interpreter', 'latex');
-ylabel('Power/Frequency (dB/Hz)', 'Interpreter', 'latex');
-grid on;
+subplot(3,1,3)
+hold on;
+% PSD def1
+plot(f_pxx1, 10*log10(pxx1_x), 'LineWidth', 2, 'Color', 'r', 'LineStyle', '-');
 
 % PSD def2
-subplot(4,1,4)
-plot(f_pxx2, 10*log10(pxx2_x), 'LineWidth', 1.5);
-title('Def-2 PSD of Impulse Signal $x_1(t)$', 'Interpreter', 'latex');
+plot(f_pxx2, 10*log10(pxx2_x), 'LineWidth', 2, 'Color', 'b', 'LineStyle', '--'); 
+
+title('PSD (Def-1 and Def-2) of Signal $x_1(t)$', 'Interpreter', 'latex');
 xlabel('Frequency (Hz)', 'Interpreter', 'latex');
 ylabel('Power/Frequency (dB/Hz)', 'Interpreter', 'latex');
+legend('PSD Def-1', 'PSD Def-2', 'Location', 'best');
 grid on;
-set(gcf, 'Color', 'w'); 
+hold off
+set(gcf, 'Color', 'w');
--- Assignment1/Task1/Task1_Sin.m
+++ Assignment1/Task1/Task1_Sin.m
@@ -17,8 +17,8 @@ L = 200; % Length of the signal
 t = (0:L-1)*T; % Time vector
 
 % Sinusoidal signal x2(t) with WGN w(t)
-f = 0.5; % Frequency 
-x = 0.7*sin(2*pi*f*t); % Sinusoid + WGN
+f = 200; % Frequency 
+x = 0.7*sin(2*pi*300*t)+0.2*randn(1, length(t));% Sinusoid + WGN
 
 
 
@@ -31,34 +31,34 @@ x = 0.7*sin(2*pi*f*t); % Sinusoid + WGN
 figure;
 
 % Signal sequence
-subplot(4,1,1)
-plot(t, x, 'LineWidth', 1.5);
+subplot(3,1,1)
+plot(t, x, 'b','LineWidth',  1.5);
 title('Signal Sequence $x_1(t)$', 'Interpreter', 'latex');
 xlabel('Time (s)', 'Interpreter', 'latex');
 ylabel('Amplitude', 'Interpreter', 'latex');
 grid on;
 
 % ACF
-subplot(4,1,2)
-plot(lags*T, acf_x, 'LineWidth', 1.5);
+subplot(3,1,2)
+plot(lags*T, acf_x,'b', 'LineWidth', 1.5);
 title('ACF of Impulse Signal $x_1(t)$', 'Interpreter', 'latex');
 xlabel('Time lag (s)', 'Interpreter', 'latex');
 ylabel('Amplitude', 'Interpreter', 'latex');
 grid on;
 
+
+subplot(3,1,3)
+hold on;
 % PSD def1
-subplot(4,1,3)
-plot(f_pxx1, 10*log10(pxx1_x), 'LineWidth', 1.5);
-title('Def-1 PSD of Impulse Signal $x_1(t)$', 'Interpreter', 'latex');
-xlabel('Frequency (Hz)', 'Interpreter', 'latex');
-ylabel('Power/Frequency (dB/Hz)', 'Interpreter', 'latex');
-grid on;
+plot(f_pxx1, 10*log10(pxx1_x), 'LineWidth', 2, 'Color', 'r', 'LineStyle', '-');
 
 % PSD def2
-subplot(4,1,4)
-plot(f_pxx2, 10*log10(pxx2_x), 'LineWidth', 1.5);
-title('Def-2 PSD of Impulse Signal $x_1(t)$', 'Interpreter', 'latex');
+plot(f_pxx2, 10*log10(pxx2_x), 'LineWidth', 2, 'Color', 'b', 'LineStyle', '--'); 
+
+title('PSD (Def-1 and Def-2) of Signal $x_1(t)$', 'Interpreter', 'latex');
 xlabel('Frequency (Hz)', 'Interpreter', 'latex');
 ylabel('Power/Frequency (dB/Hz)', 'Interpreter', 'latex');
+legend('PSD Def-1', 'PSD Def-2', 'Location', 'best');
 grid on;
-set(gcf, 'Color', 'w'); 
+hold off;
+set(gcf, 'Color', 'w');
