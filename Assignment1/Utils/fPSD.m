e29ab3f861cfb89ea4a44058e44c04fbaea12fa4
haoxiang01 <276063373@qq.com>
2024-02-03 21:42:22 +0000

    add psd

---
+++ Assignment1/Utils/fPSD.m
@@ -0,0 +1,19 @@
+function [pxx_x,f_pxx] = fPSD(input, Fs, type)
+
+nfft = 2048;
+f_pxx = Fs*(0:(nfft/2))/nfft;
+
+if strcmp(type, 'def1')
+    disp('def1')
+    r = input;
+    pxx_x = abs(fft(r, nfft));
+else
+    disp('def2')
+    x = input;
+    N = length(x);
+    pxx_x = abs(fft(x, nfft)).^2 ./ N;
+end
+   pxx_x = pxx_x(1:nfft/2+1);
+
+end
+
